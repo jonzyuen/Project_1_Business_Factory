de9ebf3e6e8ef1e0d7a5222af76163bf7f04452d
Get Shwifty <cjpatr0n1te@icloud.com>
2021-07-27 21:14:42 -0700

    functional api

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@
 const getAllGenres = () => {
     return fetch(`https://api.rawg.io/api/genres?key=c7ec26c3e2bb4ca79a5a70710956f2f8`)
         .then(response => response.json())
-        .then(data => console.log(data.results));
+        .then(data => data.results);
 }
 
 
@@ -13,7 +13,21 @@ const getGamesByGenre = (genre) => {
         .then(data => data.results);
 }
 
+// function temp() {
+//     fetch(`https://cors-anywhere.herokuapp.com/https://www.giantbomb.com/api/search/?api_key=85b938020fcf46bb6d3e1014180e6954cd8c1a1c&format=json&query=${game}`)
+//       .then(response => response.json())
+//       .then(data => data)
+// }
+// let game = 'Destiny-2'
+// console.log(temp())
 
+function temp() {
+    return fetch(`https://cors-anywhere.herokuapp.com/https://www.giantbomb.com/api/search/?api_key=85b938020fcf46bb6d3e1014180e6954cd8c1a1c&format=json&query=${game}`)
+}
+let game = 'World-of-Warcraft'
+temp()
+  .then(res => res.json())
+  .then(data => console.log(data));
 
 getAllGenres().then((genres) => {
     // genres is array of all genres
@@ -25,5 +39,4 @@ getGamesByGenre("Action").then((results) => {
     //Do stuff with results of query. Results holds array of all games for a certain genre
     console.log(results)
 })
-
 
